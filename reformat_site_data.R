7d808077b1044926813dbdc9fe38441428a1bb68
Alex <aj.jakucewicz@gmail.com>
2020-10-16 11:27:14 -0500

    Create reformat_site_data.R

---
+++ reformat_site_data.R
@@ -0,0 +1,12 @@
+reformat_site_data <- function(data){
+  for (x in length(data)) {
+    data[[x]]$tmax <- (data[[x]]$tmax2m - 273.15)
+    data[[x]]$tmin <- (data[[x]]$tmin2m - 273.15)
+    data[[x]]$tdew <- (data[[x]]$tmp2m - 273.15) - ((100 - data[[x]]$rh2m)/5.)
+    data[[x]]$prcp <- (data[[x]]$apcpsfc)
+    data[[x]]$wind <- (data[[x]]$wind_speed)
+    data[[x]]$srad <- (data[[x]]$dswrfsfc)
+    data[[x]]$tavg <- ((data[[x]]$tmax + data[[x]]$tmin) / 2)
+  }
+  return(data)
+}
