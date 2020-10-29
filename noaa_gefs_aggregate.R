1471efaf551fc61cd20503238725dec4cfe1b98e
Alexandra Jakucewicz <aj.jakucewicz@gmail.com>
2020-10-28 20:42:26 -0500

    Update noaa_gefs_aggregate.R

--- noaa_gefs_aggregate.R
+++ noaa_gefs_aggregate.R
@@ -1,68 +1,17 @@
-tmax_data <- data[[1]][, c("time", “tmax”)]
-tmin_data <- data[[1]][, c("time", “tmin”)]
-tdew_data <- data[[1]][, c("time", "tdew")]
-prcp_data <- data[[1]][, c("time", “prcp”)]
-wind_data <- data[[1]][, c("time", “wind”)]
-srad_data <- data[[1]][, c("time", “srad”)]
-tavg_data <- data[[1]][, c("time", “tavg”)]
+vars <- c("tmax", "tmin", "prcp", "tdew", "wind", "srad", "tavg")
 
-tmax_data <-
-     Agg.t(
-         data = tmax_data,
-         nameData = "tmax",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = max, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
-tmin_data <-
-    Agg.t(
-         data = tmin_data,
-         nameData = "tmin",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = min, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
+funcs <- c("max", "min", "mean", "sum", "mean", "sum", "mean")
 
-tdew_data <-
-      Agg.t(
-         data = tdew_data,
-         nameData = "tdew",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = mean, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
-
-prcp_data <-
-     Agg.t(
-         data = prcp_data,
-         nameData = "prcp",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = sum, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
-
-wind_data <-
-     Agg.t(
-         data = wind_data,
-         nameData = "wind",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = mean, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
-
-srad_data <-
-     Agg.t(
-         data = srad_data,
-         nameData = "srad",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = sum, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
-     )
-
-tavg_data <-
-    Agg.t(
-         data = tavg_data,
-         nameData = "tavg",
-         delta = 1440 , # A numeric value that specifies the level of aggregation required in minutes.
-         func = mean, # The name of the function of aggregation e.g. mean, sum.
-         namePlot = "Temporal aggregation of temperature min tmin"
+for (x in seq_len(length(data))) {
+  for (i in seq_len(length(vars))) {
+    data[[x]][, c("time", vars[i] )] <- Agg.t(
+      data = data[[x]][, c("time", vars[i])],
+      nameData = vars[i],
+      delta = 1440,
+      # A numeric value that specifies the level of aggregation required in minutes.
+      func = funcs[i],
+      # The name of the function of aggregation e.g. mean, sum.
+      namePlot = "Temporal aggregation of variable."
+    )
+  }
+}
