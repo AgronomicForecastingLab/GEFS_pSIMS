f2f971c4c0f80a31e59e3a65d7553665bec21515
Alexandra Jakucewicz <aj.jakucewicz@gmail.com>
2020-12-18 15:59:29 -0600

    Update reformat_site_data.R

--- reformat_site_data.R
+++ reformat_site_data.R
@@ -1,12 +1,12 @@
 reformat_site_data <- function(data){
   for (x in seq_len(length(data))) {
-    data[[x]]$tmax <- (data[[x]]$tmax2m - 273.15)
-    data[[x]]$tmin <- (data[[x]]$tmin2m - 273.15)
-    data[[x]]$tdew <- (data[[x]]$tmp2m - 273.15) - ((100 - data[[x]]$rh2m)/5.)
-    data[[x]]$prcp <- (data[[x]]$apcpsfc)
-    data[[x]]$wind <- (data[[x]]$wind_speed)
-    data[[x]]$srad <- (data[[x]]$dswrfsfc)
-    data[[x]]$tavg <- ((data[[x]]$tmax + data[[x]]$tmin) / 2)
+    data[[x]]$tmax <- (data[[x]]$mx2t - 273.15)
+    data[[x]]$tmin <- (data[[x]]$mn2t - 273.15)
+    data[[x]]$tdew <- (data[[x]]$d2m- 273.15)
+    data[[x]]$tavg <- (data[[x]]$t2m)
+    data[[x]]$prcp <- (data[[x]]$tp)
+    data[[x]]$srad <- (data[[x]]$ssrd)
+    data[[x]]$wind <- sqrt((data[[x]]$u10)^2 + (data[[x]]$v10)^2)
   }
   return(data)
 }
