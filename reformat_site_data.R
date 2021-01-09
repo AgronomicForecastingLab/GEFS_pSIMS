dc332f7e171da0d65687260e2e4560091988c8f0
Alexandra Jakucewicz <aj.jakucewicz@gmail.com>
2021-01-08 18:16:16 -0600

    Update reformat_site_data.R

--- reformat_site_data.R
+++ reformat_site_data.R
@@ -10,3 +10,16 @@ reformat_site_data <- function(data) {
   }
   return(data)
 }
+
+reformat_NOAA_data <- function(data) {
+  for (x in seq_len(length(data))) {
+    data[[x]]$tmax <- (data[[x]]$tmax2m - 273.15)
+    data[[x]]$tmin <- (data[[x]]$tmin2m - 273.15)
+    data[[x]]$tdew <- (data[[x]]$tmp2m - 273.15) - ((100 - data[[x]]$rh2m)/5.)
+    data[[x]]$prcp <- (data[[x]]$apcpsfc)
+    data[[x]]$wind <- sqrt((data[[x]]$ugrd10m)^2 + (data[[x]]$vgrd10m)^2)
+    data[[x]]$srad <- (data[[x]]$dswrfsfc)
+    data[[x]]$tavg <- ((data[[x]]$tmax + data[[x]]$tmin) / 2)
+  }
+  return(data)
+}
