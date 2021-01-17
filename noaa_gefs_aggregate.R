f68d3f0096ae3682c213529ac68a7ead428e67ed
Alexandra Jakucewicz <aj.jakucewicz@gmail.com>
2021-01-17 15:41:58 -0600

    Update noaa_gefs_aggregate.R

--- noaa_gefs_aggregate.R
+++ noaa_gefs_aggregate.R
@@ -4,16 +4,18 @@ vars <- c("tmax", "tmin", "prcp", "tdew", "wind", "srad", "tavg")
 funcs <- c("max", "min", "mean", "sum", "mean", "sum", "mean")
 
 agg_site_data <- function(data) {
-  for (i in seq_len(length(vars))) {
-    data[, c("time", vars[i] )] <- Agg.t(
-      data = data[, c("time", vars[i] )],
-      nameData = vars[i],
-      delta = 1440,
-      # A numeric value that specifies the level of aggregation required in minutes.
-      func = funcs[i],
-      # The name of the function of aggregation e.g. mean, sum.
-      namePlot = "Temporal aggregation of variable."
-    )
+  for (x in seq_len(length(data))) {
+    for (i in seq_len(length(vars))) {
+      data[[x]][, c("time", vars[i])] <- Agg.t(
+        data = data[[x]][, c("time", vars[i])],
+        nameData = vars[i],
+        delta = 1440,
+        # A numeric value that specifies the level of aggregation required in minutes.
+        func = funcs[i],
+        # The name of the function of aggregation e.g. mean, sum.
+        namePlot = "Temporal aggregation of variable."
+      )
+    }
   }
   return(data)
 }
