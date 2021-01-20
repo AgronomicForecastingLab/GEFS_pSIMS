516cfaf7d99ebccb801d0b7f8315e4388a4d896a
Alexandra Jakucewicz <aj.jakucewicz@gmail.com>
2021-01-20 11:32:07 -0600

    Update merge_NOAA_ERA.R

--- merge_NOAA_ERA.R
+++ merge_NOAA_ERA.R
@@ -5,8 +5,9 @@ bind_NOAA_ERA <- function(start) {
   NOAA_data <- agg_site_data(NOAA_data)
   
   # request/reformat ERA data
-  era_df_list <- era_ncfile_req(start, system.date)
+  era_df_list <- era5_ncfile_req(start, Sys.Date(), sites[1:5, ])
   era_data <- reformat_ERA_data(era_df_list)
   # bind together NOAA & ERA
   merged <- merge(era_data, NOAA_data)
+  return(merged)
 }
