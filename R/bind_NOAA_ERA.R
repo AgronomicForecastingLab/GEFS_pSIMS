3c33ef96b91a8791b8b0ebed44db626e334c84c5
Alex <aj.jakucewicz@gmail.com>
2021-03-19 22:39:18 +0000

    Add inst/, workflow demo

--- R/bind_NOAA_ERA.R
+++ R/bind_NOAA_ERA.R
@@ -5,16 +5,7 @@
 #' @param start Start date of dates requested
 #' @return List of merged dataframes.
 #' @export
-bind_NOAA_ERA <- function(start) {
-  # request/reformat NOAA data
-  NOAA_data <- noaa_gefs_request()
-  NOAA_data <- reformat_NOAA_data(NOAA_data)
-  NOAA_data <- agg_site_data(NOAA_data)
-  
-  # request/reformat ERA data
-  era_df_list <- era5_ncfile_req(start, Sys.Date(), sites[1:5, ])
-  era_data <- reformat_ERA_data(era_df_list)
-  # bind together NOAA & ERA
+bind_NOAA_ERA <- function(era_data, NOAA_data) {
   merged <- merge(era_data, NOAA_data)
   return(merged)
 }
