0b8f91791b12cf39c1a5b741edd5a6aa2439b7cf
Hamze Dokoohaki <para2x@gmail.com>
2021-04-21 17:30:52 +0000

    Fixing the aggregate

--- R/era5_ncfile_request.R
+++ R/era5_ncfile_request.R
@@ -1,11 +1,12 @@
 #' Request ERA5 hourly weather data ncfiles
 #'
+#' @param user character, username
 #' @param sdate Start date of hourly data
 #' @param edate End date of hourly data
 #' @param sites A dataframe of sites (ID, lat, lon)
 #' @return A list of site var data frames
 #' @export
-era5_ncfile_request <- function(sdate, edate, sites) {
+era5_ncfile_request <- function(user, sdate, edate, sites) {
   df_list <- vector("list", nrow(sites))
   for (r in 1:nrow(sites)) {
     site_coord <- c(sites[r, 'Lat'],
@@ -14,12 +15,16 @@ era5_ncfile_request <- function(sdate, edate, sites) {
                     sites[r, 'Lon'])
     dates <- seq(as.Date(sdate), as.Date(edate), by="days")
     req <- era5_request(site_coord, dates)
-    ncfile <- wf_request(user = "67047",
+    ncfile <- wf_request(user = user,
                          request = req,
                          transfer = TRUE,
                          path = "~",
                          verbose = FALSE)
-    df_list[[r]] <- as.data.frame(read_ncdf(ncfile))
+    df_list[[r]] <- as.data.frame(stars::read_ncdf(ncfile))
+
+    #When reading the ncdf file, there is usually an issue with time. We fix that here
+    error_in_time <- as.POSIXct(sdate) - df_list[[r]]$time[1]
+    df_list[[r]]$time <- df_list[[r]]$time + error_in_time
   }
   return(df_list)
 }
--- R/noaa_gefs_aggregate.R
+++ R/noaa_gefs_aggregate.R
@@ -1,6 +1,6 @@
 #' Aggregate NOAA GEFS to 6-hour step
 #'
-#' Aggregates NOAA GEFS weather variable data from a daily to 6-hour
+#' Aggregates NOAA GEFS weather variable data from a 6-hour to daily
 #' time step.
 #'
 #' @param data List of site weather dataframes
@@ -8,22 +8,26 @@
 #' @export
 noaa_gefs_aggregate <- function(data) {
   vars <- c("tmax", "tmin", "prcp", "tdew", "wind", "srad",  "tavg")
-  funcs <- c("max", "min", "mean", "sum", "mean", "sum", "mean")
+  funcs <- c("max", "min", "mean", "mean", "mean", "sum", "mean")
   for (x in seq_len(length(data))) {
     # Removes NA values from df
     temp <- na.omit(data[[x]])
-    for (i in seq_len(length(vars))) {
-      temp[, c("time", vars[i])] <- stUPscales::Agg.t(
-        data = temp[, c("time", vars[i])],
-        nameData = vars[i],
+
+    data[[x]]<-purrr::map2_dfc(vars, funcs, function(var, func){
+
+     stUPscales::Agg.t(
+        data = temp[, c("time", var)],
+        nameData = var,
         delta = 1440,
         # A numeric value that specifies the level of aggregation required in minutes.
-        func = funcs[i],
+        func = func,
         # The name of the function of aggregation e.g. mean, sum.
         namePlot = "Temporal aggregation of variable."
-      )
-    }
-    data[[x]] <- temp
+      ) %>%
+        select(var)
+    }) %>%
+      bind_cols(time=unique(as.Date(temp$time)))
+
   }
   return(data)
 }
